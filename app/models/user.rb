40019a738fbd8bc30ecfa65bb341c266c8df960d
Boondock19 <josematiasgonzalez20@gmail.com>
2023-10-01 02:13:03 -0400

    Manejado cambio dinamico de nombre de campo en el form

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,8 @@ class User < ApplicationRecord
   belongs_to :person_type
   validates :indentificationNumber, presence: true, format: { with: /\A[+-]?\d+\z/, message: "Only numbers allowed" },uniqueness: true,
   length: {minimum: 7, maximum: 12, too_short: "Must have at least 7 numbers", too_long: "Must have at most 12 numbers"}
-  validates :startDate, presence: true , comparison: { less_than: :endDate, message: "Must be less than end date"}
-  validates :endDate, presence: true, comparison: { greater_than: :startDate, message: "Must be greater than start date"}
+  validates :startDate, presence: true , comparison: { less_than: :endDate, message: "Must be less than document end date"}
+  validates :endDate, presence: true, comparison: { greater_than: :startDate, message: "Must be greater than document start date"}
   validates :name, presence: true, format: { with: /\A[^0-9`!@#"\{\}\[\]\$%\^&*+_=]+\z/, message: "Only letters allowed"  },
   length: {minimum: 3, maximum: 30, too_short: "Must have at least 4 letters", too_long: "Must have at most 30 letters"}
   validates :email, presence: true ,format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "invalid"  }, 
