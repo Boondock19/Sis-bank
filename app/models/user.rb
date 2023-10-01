637fd5a578bc5648114835a99bc78601a0e023cc
Boondock19 <josematiasgonzalez20@gmail.com>
2023-10-01 01:25:16 -0400

    Cambiado nombre de campos

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ApplicationRecord
-  belongs_to :person_type, optional: true
+
+  HUMANIZED_ATTRIBUTES = {
+    :indentificationNumber => "Id Number",
+    :startDate => "Document Start Date",
+    :endDate => "Document End Date",
+    :mainNumber => "Main Number",
+    :optNumber  => "Secondary Number",
+  }
+
+  belongs_to :person_type
   validates :indentificationNumber, presence: true, format: { with: /\A[+-]?\d+\z/, message: "Only numbers allowed" },uniqueness: true,
   length: {minimum: 7, maximum: 12, too_short: "Must have at least 7 numbers", too_long: "Must have at most 12 numbers"}
   validates :startDate, presence: true , comparison: { less_than: :endDate, message: "Must be less than end date"}
@@ -9,6 +18,12 @@ class User < ApplicationRecord
   validates :email, presence: true ,format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "invalid"  }, 
     uniqueness: { case_sensitive: false }, 
     length: { minimum: 4, maximum: 254 }
-  validates :mainNumber, presence: true
+  validates :mainNumber, presence: true, format: { with: /\A[+-]?\d+\z/, message: "Only numbers allowed" }
+  validates :optNumber, format: { with: /\A[+-]?\d+\z/, message: "Only numbers allowed" }, if: Proc.new { |a| !a.optNumber.empty? }
   validates :person_type_id, presence: true
+
+  def self.human_attribute_name(attr, options = {}) # 'options' wasn't available in Rails 3, and prior versions.
+    HUMANIZED_ATTRIBUTES[attr.to_sym] || super
+  end
+
 end
